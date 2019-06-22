dbddc86437404febc0e2eaea890e6617f9c238dd
unknown <mariofph@gmail.com.com>
2019-06-22 09:01:04 -0600

    Setting up HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 
 function component() {
+    const parentElement = document.createElement('section');
     const element = document.createElement('div');
     const myIcon = new Image();
+    const btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -13,8 +16,14 @@ function component() {
     // Add the image to our existing div.    
     myIcon.src = Icon;
     element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    parentElement.appendChild(element);
+    parentElement.appendChild(btn);
+
     console.log(Data);
-    return element;
+    return parentElement;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     module: {
         rules: [{
                 test: /\.css$/,
